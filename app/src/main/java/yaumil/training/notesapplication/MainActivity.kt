8d946cba1767f780de7b89916ec4879ea8ebbdfc
yaumil <yaumilramkhadani@gmail.com>
2022-10-20 19:15:48 +0700

    Firebase firestore integrations

--- app/src/main/java/yaumil/training/notesapplication/MainActivity.kt
+++ app/src/main/java/yaumil/training/notesapplication/MainActivity.kt
@@ -2,6 +2,8 @@ package yaumil.training.notesapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.FirebaseFirestore
 import yaumil.training.notesapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,5 +13,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val user: MutableMap<String, Any> = HashMap()
+        user["first"] = "Muhammad"
+        user["last"] = "Yaumil Ramadhani"
+        user["born"] = 2001
+
+        FirebaseFirestore.getInstance().collection("users")
+            .add(user)
+            .addOnSuccessListener { documentReference ->
+                Log.d("TAB","DocumentSnapshot add with ID : " + documentReference.id) }
+            .addOnFailureListener { e -> Log.w("TAB","Error adding document",e) }
+
     }
 }
